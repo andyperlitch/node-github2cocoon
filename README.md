ec1be59bc2e12502562ea4e5545468a8d530c273
Andy Perlitch <andyperlitch@gmail.com>
2014-04-27 16:17:37 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-node-github2cocoon
-==================
+github2cocoon
+=============
+Express.js middleware to use a github repository/branch as a zip file endpoint for the [CocoonJS Launcher](http://support.ludei.com/hc/en-us/articles/201048463-How-to-use).
 
-Express.js middleware to use a github repository/branch as a zip file endpoint for the CocoonJS Launcher.
+motivation
+----------
+Ludei's CocoonJS Launcher allows you to specify a URL endpoint for a zip containing your app. I wanted to use Github's archive endpoints (e.g. https://github.com/[USERNAME]/[REPO]/archive/[BRANCH].zip) for this purpose so that I could push to GH and then immediately test on my phone. This turns out not to work because Github packages the zip archive in a way that cocoon cannot find and execute the proper files. This middleware transforms the zip archive in a cocoon-compatible way.
+
+usage
+-----
+
+```JavaScript
+// Run this node server at an endpoint your 
+// phone with cocoonjs launcher can reach.
+var express = require('express');
+var app = express();
+var github2cocoon = require("github2cocoon");
+
+app.use(github2cocoon("/zips/", { username: "andyperlitch" }));
+app.listen(80);
+```
+
+Then, put your server url into the Launcher:
+![CocoonJS Launcher screenshot](/screenshot.png)
---
+++ index.js
@@ -0,0 +1,70 @@
+var unzip = require('unzip');
+var request = require('request');
+var archiver = require('archiver');
+
+function factory(root, options) {
+
+    if (!root || typeof root !== 'string') {
+        throw new Error('root path required (first argument, e.g.: app.use(github2cocoon("/ludei_zips/", { username: "andyperlitch" }));');
+    }
+
+    if (!options || !options.username) {
+        throw new Error('you must provide a github username, e.g.: app.use(github2cocoon("/ludei_zips/", { username: "andyperlitch" }));');
+    }
+
+    // Store the username
+    var username = options.username;
+
+    // Ensure that root starts with a leading slash
+    root = root.replace(/^\/*/, '/');
+
+    // RegEx to test paths
+    var exp = new RegExp('^' + root + '([^\/]+)/([^\/]+)');
+
+    return function(req, res, next) {
+
+        var url = req.url;
+
+        var result = exp.exec(url);
+
+        if (!result) {
+            return next();
+        }
+
+        // repo, branch
+        var repo = result[1];
+        var branch = result[2];
+
+        // The archive stream
+        var archive = archiver('zip');
+
+        // Will hold name of parent directory
+        var rootDirName;
+
+        // Get input, pipe to unzip utility
+        var input = request('https://github.com/' + username + '/' + repo + '/archive/' + branch).pipe(unzip.Parse());
+
+        // Pass all but our parent directory to
+        // archiver.
+        input.on('entry', function(entry) {
+            var filepath = entry.path;
+            if (!rootDirName) {
+                rootDirName = filepath;
+                entry.autodrain();    
+            } else {
+                // Strip name of parent dir
+                archive.append(entry, { name: entry.path.replace(rootDirName, '') });
+            }
+        });
+
+        // Finalize the archive when input has closed.
+        input.on('close', function() {
+            archive.finalize();
+        });
+
+        // Emit to response
+        archive.pipe(res);
+    }
+
+}
+exports = module.exports = factory;
